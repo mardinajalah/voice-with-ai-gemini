5c9db1d53c283e4cbb8264930570a71e2579ac53
mardinajalah <tredingb25@gmail.com>
2025-02-04 05:31:15 +0700

    memperbaiki file src/components/RecordingView.tsx dan file src/api/route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,17 +1,14 @@
 import { NextResponse, NextRequest } from "next/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export async function POST(req: NextRequest) {
-  const { prompt } = await req.json()
-  try {
-    const result = await model.generateContent(prompt);
-    const geminiResponse = result.response.text();
-    return NextResponse.json({
-      response: geminiResponse
-    });
-  } catch (error: any) {
-    return NextResponse.json({ error });
-  }
+  const { prompt } = await req.json();
+  const result = await model.generateContent(prompt);
+  const geminiResponse = result.response.text();
+  return NextResponse.json({
+    response: geminiResponse,
+  });
 }
